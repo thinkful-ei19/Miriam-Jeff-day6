d01a1172f495108ab0a533e7732298e407dc0bf8
Miriam Heller <miriam.heller@gmail.com>
2018-02-26 14:16:29 -0500

    trying to sync

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,10 +1,3 @@
 const Item = (function(){
     return Item;
 } );
-// Factory functions are functions which return an object
-function createName(name) {
-    const name = name;
-    const id = cuid();
-    const checked = false;
-    return name, id, checked;
-}
--- scripts/store.js
+++ scripts/store.js
@@ -1,3 +1,4 @@
+/* global cuid */
 
 const store = (function () {
   const foo = 'bar';
